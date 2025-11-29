b8f87a56d8b91691402ff3843a549218b6adafb7
Shivam Garade <shivamgarade05@gmail.com>
2025-11-29 13:36:26 +0530

    implement dock presentation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import Navbar from '#components/Navbar'
 import React from 'react'
+import {Navbar,Welcome,Dock} from '#components/index'
+
 
 const App = () => {
   return (
     <main>
       <Navbar/>
+      <Welcome/>
+      <Dock/>
     </main>
   )
 }
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import Navbar from '#components/Navbar'
+import Welcome from '#components/Welcome'
+import Dock from '#components/dock'
+
+
+export {Navbar,Welcome,Dock};
