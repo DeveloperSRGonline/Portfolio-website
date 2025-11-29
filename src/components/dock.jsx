89d26c5ac9a6798fc300c905d6b1ee144962cc23
Shivam Garade <shivamgarade05@gmail.com>
2025-11-29 14:50:27 +0530

    implemented window handling logic

--- src/components/dock.jsx
+++ src/components/dock.jsx
@@ -3,60 +3,71 @@ import { useGSAP } from '@gsap/react';
 import React, { useRef } from 'react'
 import { Tooltip } from 'react-tooltip'
 import gsap from 'gsap'
+import useWindowStore from '#store/window';
 
 
 const Dock = () => {
     const dockRef = useRef(null);
+    const { openWindow, closeWindow, windows } = useWindowStore()
 
     useGSAP(() => {
         const dock = dockRef.current;
-        if(!dock) return;
+        if (!dock) return;
 
         const icons = dock.querySelectorAll('.dock-icon');
 
         const animateIcons = (mouseX) => {
-            const {left} = dock.getBoundingClientRect();
-            
-            icons.forEach((icon)=> {
-                const {left:iconLeft,width} = icon.getBoundingClientRect();
+            const { left } = dock.getBoundingClientRect();
 
-                const center = iconLeft - left + width/2;
+            icons.forEach((icon) => {
+                const { left: iconLeft, width } = icon.getBoundingClientRect();
+
+                const center = iconLeft - left + width / 2;
                 const distance = Math.abs(mouseX - center);
 
-                const intensity =  Math.exp(-(distance ** 2.5)/20000)
+                const intensity = Math.exp(-(distance ** 2.5) / 20000)
 
-                gsap.to(icon,{
-                    scale:1 + 0.25 * intensity,
-                    y:-15 * intensity,
-                    duration:0.2,
-                    ease:'power1.out'
+                gsap.to(icon, {
+                    scale: 1 + 0.25 * intensity,
+                    y: -15 * intensity,
+                    duration: 0.2,
+                    ease: 'power1.out'
                 })
             })
         }
 
         const handleMouseMove = (e) => {
-            const {left} = dock.getBoundingClientRect();
+            const { left } = dock.getBoundingClientRect();
             animateIcons(e.clientX - left)
         }
 
-        const resetIcons = () => icons.forEach((icon) => gsap.to(icon,{
-            scale:1,
-            y:0,
-            duration:0.3,
-            ease:'power1.out'
+        const resetIcons = () => icons.forEach((icon) => gsap.to(icon, {
+            scale: 1,
+            y: 0,
+            duration: 0.3,
+            ease: 'power1.out'
         }))
 
-        dock.addEventListener('mousemove',handleMouseMove)
-        dock.addEventListener('mouseleave',resetIcons)
+        dock.addEventListener('mousemove', handleMouseMove)
+        dock.addEventListener('mouseleave', resetIcons)
 
         return () => {
-            dock.removeEventListener('mousemove',handleMouseMove)
-            dock.removeEventListener('mouseleave',resetIcons)
+            dock.removeEventListener('mousemove', handleMouseMove)
+            dock.removeEventListener('mouseleave', resetIcons)
         }
-    },[])
+    }, [])
 
-    const toggleApp = (app) => { 
-        
+    const toggleApp = (app) => {
+        if (!app.canOpen) return;
+
+        const window = windows[app.id]
+        if (window.isOpen) {
+            closeWindow(app.id)
+        } else {
+            openWindow(app.id)
+        }
+
+        console.log(windows)
     }
     return (
         <section id='dock' >
--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,6 @@ import Navbar from './Navbar'
 import Welcome from './Welcome'
 
 
-export { Navbar, Welcome };
+import Dock from './dock'
+
+export { Navbar, Welcome, Dock };
